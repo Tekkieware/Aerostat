045c16558a66377e0cd052add93179e558586a33
Isaiah Ozadhe <tekkieware@gmail.com>
2025-05-29 16:43:34 +0100

    add success notification to preference saving

--- components/settings-form.tsx
+++ components/settings-form.tsx
@@ -7,6 +7,7 @@ import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
 import { Switch } from "@/components/ui/switch"
 import { useForm } from "react-hook-form"
 import { useTheme } from "next-themes"
+import { CheckCircle } from "lucide-react"
 
 interface SettingsFormValues {
   units: string
@@ -17,7 +18,8 @@ export default function SettingsForm() {
   const [isSaving, setIsSaving] = useState(false)
   const { setTheme, theme } = useTheme()
   const [temperatureUnit, setTemperatureUnit] = useState<string>()
-  const [mounted, setMounted] = useState(false)  
+  const [mounted, setMounted] = useState(false)
+  const [success, setSuccess] = useState(false)
 
   const form = useForm<SettingsFormValues>({
     defaultValues: {
@@ -26,11 +28,11 @@ export default function SettingsForm() {
     },
   })
 
-  useEffect(()=>{
+  useEffect(() => {
     setMounted(true)
     const savedUnit = localStorage.getItem('temperature-unit') || "C"
     setTemperatureUnit(savedUnit)
-  },[])
+  }, [])
 
 
   useEffect(() => {
@@ -43,92 +45,115 @@ export default function SettingsForm() {
   }, [mounted, temperatureUnit, theme, form])
 
   function onSubmit(data: SettingsFormValues) {
-    setIsSaving(true)
-    if(theme != data.darkMode){
-      setTheme(data.darkMode)
+    setIsSaving(true);
+
+    let hasChanges = false;
+
+    if (theme !== data.darkMode) {
+      setTheme(data.darkMode);
+      hasChanges = true;
     }
-    if(temperatureUnit != data.units){
-      localStorage.setItem('temperature-unit', data.units)
+
+    if (temperatureUnit !== data.units) {
+      localStorage.setItem('temperature-unit', data.units);
+      hasChanges = true;
     }
-    setIsSaving(false)
+
+    if (hasChanges) {
+      setSuccess(true);
+      setTimeout(() => {
+        setSuccess(false);
+      }, 5000);
+    }
+
+    setIsSaving(false);
   }
+
 
   return (
     <>
-  {mounted && 
+      {mounted &&
 
-    <Form {...form}>
-    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 md:space-y-8">
-      <FormField
-        control={form.control}
-        name="units"
-        render={({ field }) => (
-          <FormItem className="space-y-3">
-            <FormLabel className="text-base">Temperature Units</FormLabel>
-            <FormControl>
-              <RadioGroup
-                onValueChange={field.onChange}
-                value={field.value}
-                defaultValue={field.value}
-                className="flex flex-col space-y-1"
-              >
-                <FormItem className="flex items-center space-x-3 space-y-0">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 md:space-y-8">
+            <FormField
+              control={form.control}
+              name="units"
+              render={({ field }) => (
+                <FormItem className="space-y-3">
+                  <FormLabel className="text-base">Temperature Units</FormLabel>
                   <FormControl>
-                    <RadioGroupItem value="C" />
+                    <RadioGroup
+                      onValueChange={field.onChange}
+                      value={field.value}
+                      defaultValue={field.value}
+                      className="flex flex-col space-y-1"
+                    >
+                      <FormItem className="flex items-center space-x-3 space-y-0">
+                        <FormControl>
+                          <RadioGroupItem value="C" />
+                        </FormControl>
+                        <FormLabel className="font-normal text-sm md:text-base">Celsius (°C)</FormLabel>
+                      </FormItem>
+                      <FormItem className="flex items-center space-x-3 space-y-0">
+                        <FormControl>
+                          <RadioGroupItem value="F" />
+                        </FormControl>
+                        <FormLabel className="font-normal text-sm md:text-base">Fahrenheit (°F)</FormLabel>
+                      </FormItem>
+                    </RadioGroup>
                   </FormControl>
-                  <FormLabel className="font-normal text-sm md:text-base">Celsius (°C)</FormLabel>
                 </FormItem>
-                <FormItem className="flex items-center space-x-3 space-y-0">
-                  <FormControl>
-                    <RadioGroupItem value="F" />
-                  </FormControl>
-                  <FormLabel className="font-normal text-sm md:text-base">Fahrenheit (°F)</FormLabel>
-                </FormItem>
-              </RadioGroup>
-            </FormControl>
-          </FormItem>
-        )}
-      />
+              )}
+            />
 
-      <FormField
-        control={form.control}
-        name="darkMode"
-        render={({ field }) => (
-          <FormItem className="space-y-3">
-            <FormLabel className="text-base">Theme Preference</FormLabel>
-            <FormControl>
-              <RadioGroup
-                onValueChange={field.onChange}
-                defaultValue={field.value}
-                className="flex flex-col space-y-1"
-              >
-                <FormItem className="flex items-center space-x-3 space-y-0">
+            <FormField
+              control={form.control}
+              name="darkMode"
+              render={({ field }) => (
+                <FormItem className="space-y-3">
+                  <FormLabel className="text-base">Theme Preference</FormLabel>
                   <FormControl>
-                    <RadioGroupItem value="light" />
+                    <RadioGroup
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                      className="flex flex-col space-y-1"
+                    >
+                      <FormItem className="flex items-center space-x-3 space-y-0">
+                        <FormControl>
+                          <RadioGroupItem value="light" />
+                        </FormControl>
+                        <FormLabel className="font-normal text-sm md:text-base">Light Mode</FormLabel>
+                      </FormItem>
+                      <FormItem className="flex items-center space-x-3 space-y-0">
+                        <FormControl>
+                          <RadioGroupItem value="dark" />
+                        </FormControl>
+                        <FormLabel className="font-normal text-sm md:text-base">Dark Mode</FormLabel>
+                      </FormItem>
+                    </RadioGroup>
                   </FormControl>
-                  <FormLabel className="font-normal text-sm md:text-base">Light Mode</FormLabel>
                 </FormItem>
-                <FormItem className="flex items-center space-x-3 space-y-0">
-                  <FormControl>
-                    <RadioGroupItem value="dark" />
-                  </FormControl>
-                  <FormLabel className="font-normal text-sm md:text-base">Dark Mode</FormLabel>
-                </FormItem>
-              </RadioGroup>
-            </FormControl>
-          </FormItem>
-        )}
-      />
+              )}
+            />
 
-      <div className="flex justify-end">
-        <Button type="submit" disabled={isSaving}>
-          {isSaving ? "Saving..." : "Save Settings"}
-        </Button>
-      </div>
-    </form>
-  </Form>
-  }
-  </>
+            <div className="flex justify-between">
+              <div className="flex gap-1 items-center animate-pulse">
+                {success &&
+                  <>
+                    <CheckCircle color="#16a34a" size={20} />
+                    <p className=" text-green-600 font-semibold text-sm">Preference saved</p>
+                  </>
+                }
+              </div>
+              <Button type="submit" disabled={isSaving}>
+                {isSaving ? "Saving..." : "Save Settings"}
+              </Button>
+            </div>
+          </form>
+        </Form>
+      }
+    </>
   )
 }
 
